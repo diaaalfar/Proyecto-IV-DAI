1a1d7d97949328be27b5a00f49c8ccc5d61e6d8d
JesGor <jesusgorillo@gmail.com>
2015-11-15 18:35:54 +0100

    Organizando el directorio de la app

---
+++ scrumpy/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Proyecto, Seccion, Tarea
+
+class crearProyectoForm(forms.ModelForm):
+	class Meta:
+		model = Proyecto
+		fields = ( "nombre", "desc", "url")
+		
+class crearSeccionForm(forms.ModelForm):
+	class Meta:
+		model = Seccion
+		fields = ( "nombre", "desc", "proyecto" )
+
+class crearTareaForm(forms.ModelForm):
+	class Meta:
+		model = Tarea
+		fields = ( "nombre", "desc", "estado", "seccion" )
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name="scrumpy",
+	version="0.0.1",
+	description="Aplicación web, creada con python y django, para la organización de las tareas del equipo de trabajo de un proyecto",
+	url="https://github.com/JesGor/Proyecto-IV-DAI",
+	author="Jesus Prieto Lopez",
+	author_email="jesusgorillo@gmail.com",
+	license="GNU General Public License",
+	packages=["proyecto"],
+	install_requires=[
+		"django",
+		"wheel",
+	],
+	zip_safe=False
+)
