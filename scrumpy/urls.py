7d3c48b17c08e6a31dc47369626c3744fc0ceb2b
JesGor <jesusgorillo@gmail.com>
2015-11-24 20:34:01 +0100

    Borrando carpetas temporalmente para mejor organización

--- scrumpy/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from scrumpy import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^proyectos/$', views.lista_proyectos),
-    url(r'^proyecto/(?P<pk>[0-9]+)/$', views.proyecto),
-]
