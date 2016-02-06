aa3fbadea3be7465f1e83df81403a21b2e808fc1
JesGor <jesusgorillo@gmail.com>
2016-02-06 01:18:16 +0100

    Borrando archivo con nombre ambigüo

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-heroku login
-heroku create
-git add .
-git commit -m "Desplegando aplicación en heroku"
-git push heroku master
-heroku run python manage.py migrate --noinput
-heroku ps:scale web=1
