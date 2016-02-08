84927b41f059ae4a7119bcd7893c0e01ea930a28
JesGor <jesusgorillo@gmail.com>
2016-02-08 17:17:16 +0100

    Modificando script para el despliegue en azure

--- azure.sh
+++ azure.sh
@@ -8,5 +8,9 @@ sudo vagrant plugin install vagrant-azure
 sudo apt-get install -y python-pip
 #Instalar ansible
 sudo pip install paramiko PyYAML jinja2 httplib2 ansible
-#Desplegar con vagrant y provisionar con ansible en azure
+#Instalar fabric
+sudo apt-get install -y fabric
+#Creación y provisionamiento de máquina virtual mediante archivo Vagrantfile y .yml
 sudo vagrant up --provider=azure
+#Despliegue/Inicio de la aplicación a través de fabric
+sudo fab -H bareteca@bareteca.cloudapp.net run_app
